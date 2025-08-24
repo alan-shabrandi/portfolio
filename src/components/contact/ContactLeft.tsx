745c22ac0aefd657f9d49fd1cf5528289a03ac48
Alan Shabrandi <alan.shabrandi@gmail.com>
2025-08-24 22:06:09 +0330

    portfolio

--- src/components/contact/ContactLeft.tsx
+++ src/components/contact/ContactLeft.tsx
@@ -12,16 +12,16 @@ const ContactLeft: React.FC = () => {
     <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
       <img className="w-full h-64 object-cover rounded-lg mb-2" src={contact} alt="contactImg" />
       <div className="flex flex-col gap-4">
-        <h3 className="text-3xl font-bold text-white">John Doe</h3>
+        <h3 className="text-3xl font-bold text-white">Alan Shabrandi</h3>
         <p className="text-lg font-normal text-gray-400">MERN Stack Developer</p>
         <p className="text-base text-gray-400 tracking-wide">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ipsam autem cumque, accusantium dicta odio.
         </p>
         <p className="text-base text-gray-400 flex items-center gap-2">
-          Phone: <span className="text-lightText">+968 97859628</span>
+          Phone: <span className="text-lightText">+98 9911582801</span>
         </p>
         <p className="text-base text-gray-400 flex items-center gap-2">
-          Email: <span className="text-lightText">noor.jsdivs@gmail.com</span>
+          Email: <span className="text-lightText">alan.shabrandi@gmail.com</span>
         </p>
       </div>
       <div className="flex flex-col gap-4">
